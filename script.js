05bd8d94c3ce240df6b9ea80c231a9f903d4c39a
flasticky <jan.flaska@gmail.com>
2024-03-04 08:36:16 +0100

    feat: script.js - Variables creation

---
+++ script.js
@@ -0,0 +1,13 @@
+const lengthSlider = document.querySelector(".pass-length input");
+const options = document.querySelectorAll(".option input");
+const copyIcon = document.querySelector(".input-box span");
+const passwordInput = document.querySelector(".input-box input");
+const passIndicator = document.querySelector(".pass-indicator");
+const generateBtn = document.querySelector(".generate-btn");
+
+const characters = {
+    lowercase: "abcdefghijklmnopqrstuvwxyz",
+    uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    numbers: "0123456789",
+    symbols: "!@#$%^&*(){}[]-_=+:';|<>,.?/~"
+}
